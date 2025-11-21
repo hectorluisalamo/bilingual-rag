ff2431cf6ada6580c8e3d319f80c31d4ce20e95f
Hector Luis Alamo <halamo24@gmail.com>
2025-11-21 14:48:25 -0800

    ob: config structlog

---
+++ api/core/logging.py
@@ -0,0 +1,12 @@
+import structlog, logging, sys, os
+
+
+def setup_logging():
+    logging.basicConfig(stream=sys.stdout, level=os.getenv("LOG_LEVEL","INFO"))
+    structlog.configure(
+        processors=[structlog.processors.TimeStamper(fmt="iso"), structlog.processors.add_log_level, structlog.processors.JSONRenderer()],
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        cache_logger_on_first_use=True,
+    )
+    
+setup_logging()
