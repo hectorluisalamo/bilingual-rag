9872206b957b3c9487c81a74f00dc7752c6cf699
Hector Luis Alamo <halamo24@gmail.com>
2025-11-21 08:55:29 -0800

    db: add soft-delete + latest view

---
+++ migrations/004_retention.sql
@@ -0,0 +1,6 @@
+ALTER TABLE documents ADD COLUMN IF NOT EXISTS deleted BOOLEAN DEFAULT FALSE;
+CREATE OR REPLACE VIEW v_latest_docs AS
+SELECT DISTINCT ON (source_uri) *
+FROM documents
+WHERE approved = TRUE AND deleted = FALSE
+ORDER BY source_uri, ver DESC, fetched_at DESC;
