837a336641c74a38cbcadb89bb1aef28f550dd38
Hector Luis Alamo <halamo24@gmail.com>
2025-11-21 09:35:09 -0800

    fix(db): column name version not ver (documents)

--- migrations/004_retention.sql
+++ migrations/004_retention.sql
@@ -3,4 +3,4 @@ CREATE OR REPLACE VIEW v_latest_docs AS
 SELECT DISTINCT ON (source_uri) *
 FROM documents
 WHERE approved = TRUE AND deleted = FALSE
-ORDER BY source_uri, ver DESC, fetched_at DESC;
+ORDER BY source_uri, version DESC, fetched_at DESC;
